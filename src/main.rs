bbd734d4b9d61660ade7e2134c3b120784bc78f0
zhscn <zhscn@proton.me>
2024-08-16 10:45:32 +0800

    update

--- src/main.rs
+++ src/main.rs
@@ -1,126 +1,53 @@
 use std::io::Write;
 
-use regex::Regex;
 use tabled::{settings::Style, Table, Tabled};
 
-const HIGH_POOL_MASK: u64 = 0xFF << 56;
-const HIGH_SHARD_MASK: u64 = 0xFF << 48;
-const HIGH_CRUSH_MASK: u64 = 0xFFFFFFFF << 16;
-const HIGH_RANDOM_MASK: u64 = 0xFFFF;
-
-mod no_shadow {
-    pub const LOW_RANDOM_MASK: u64 = u64::MAX << 46;
+struct FieldSpec {
+    #[allow(dead_code)]
+    name: &'static str,
+    offset: usize,
+    length: usize,
 }
 
-mod has_shadow {
-    pub const LOW_RANDOM_MASK: u64 = u64::MAX << 47;
-    pub const LOW_SHADOW_MASK: u64 = 1 << 46;
-}
-
-const LOW_METADATA_MASK: u64 = 1 << 45;
-const LOW_SNAP_MASK: u64 = 1 << 44;
-const LOW_LOCAL_SNAP_ID_MASK: u64 = 0xFFFFFFFF << 12;
-const LOW_OFFSET_MASK: u64 = (1 << 12) - 1;
-
-#[derive(Debug, Clone, Copy)]
-struct Laddr {
-    low: u64,
-    high: u64,
-}
-
-impl Laddr {
-    fn pool(&self) -> u8 {
-        ((self.high & HIGH_POOL_MASK) >> 56) as u8
-    }
-    fn shard(&self) -> u8 {
-        ((self.high & HIGH_SHARD_MASK) >> 48) as u8
-    }
-    fn crush(&self) -> u32 {
-        ((self.high & HIGH_CRUSH_MASK) >> 16) as u32
-    }
-    fn random(&self, has_shadow: bool) -> u64 {
-        if has_shadow {
-            ((self.high & HIGH_RANDOM_MASK) << 17)
-                | ((self.low & has_shadow::LOW_RANDOM_MASK) >> 47)
-        } else {
-            ((self.high & HIGH_RANDOM_MASK) << 18) | ((self.low & no_shadow::LOW_RANDOM_MASK) >> 46)
+impl FieldSpec {
+    const fn new(name: &'static str, length: usize, offset: usize) -> Self {
+        Self {
+            name,
+            offset,
+            length,
         }
     }
-    fn shadow(&self) -> bool {
-        (self.low & has_shadow::LOW_SHADOW_MASK) != 0
+    const fn mask(&self) -> u128 {
+        ((1u128 << self.length) - 1) << self.offset
     }
-    fn with_shadow(&self, shadow: bool) -> Laddr {
-        let low = if shadow {
-            self.low | has_shadow::LOW_SHADOW_MASK
-        } else {
-            self.low & !has_shadow::LOW_SHADOW_MASK
-        };
-        Laddr { low, ..*self }
+    const fn get(&self, value: u128) -> u128 {
+        (value & self.mask()) >> self.offset
     }
-    fn metadata(&self) -> bool {
-        (self.low & LOW_METADATA_MASK) != 0
-    }
-    fn with_metadata(&self, metadata: bool) -> Laddr {
-        let low = if metadata {
-            self.low | LOW_METADATA_MASK
-        } else {
-            self.low & !LOW_METADATA_MASK
-        };
-        Laddr { low, ..*self }
-    }
-    fn snap(&self) -> bool {
-        (self.low & LOW_SNAP_MASK) != 0
-    }
-    fn with_snap(&self, snap: bool) -> Laddr {
-        let low = if snap {
-            self.low | LOW_SNAP_MASK
-        } else {
-            self.low & !LOW_SNAP_MASK
-        };
-        Laddr { low, ..*self }
-    }
-    fn local_snap_id(&self) -> u32 {
-        ((self.low & LOW_LOCAL_SNAP_ID_MASK) >> 12) as u32
-    }
-    fn offset(&self) -> u32 {
-        ((self.low & LOW_OFFSET_MASK) as u32) << 12
-    }
-    fn get_object_prefix(&self, has_shadow: bool) -> Laddr {
-        let low = if has_shadow {
-            self.low & has_shadow::LOW_RANDOM_MASK
-        } else {
-            self.low & no_shadow::LOW_RANDOM_MASK
-        };
-        Self { low, ..*self }
-    }
-    fn get_onode_prefix(&self) -> Laddr {
-        Laddr {
-            low: self.low & !LOW_OFFSET_MASK,
-            high: self.high,
-        }
+    #[allow(dead_code)]
+    fn set(&self, value: u128, field: u128) -> u128 {
+        (value & !self.mask()) | ((field << self.offset) & self.mask())
     }
 }
 
-impl From<&str> for Laddr {
-    fn from(s: &str) -> Self {
-        if s.starts_with("0x") {
-            let u = u128::from_str_radix(&s[2..], 16).unwrap();
-            let low = u as u64;
-            let high = (u >> 64) as u64;
-            Self { low, high }
-        } else {
-            // low = 1234, high = 5678
-            let re = Regex::new(r"low = (\d+), high = (\d+)").unwrap();
-            let caps = re.captures(s).unwrap();
-            let low: u64 = caps[1].parse().unwrap();
-            let high: u64 = caps[2].parse().unwrap();
-            Self { low, high }
-        }
-    }
-}
+// [upgrade:1][pool:12][shard:8][reverse_hash:32][local_object_id:27][is_metadata:1][object_content:47]
+const UPGRADE_SPEC: FieldSpec = FieldSpec::new("upgrade", 1, 127);
+const POOL_SPEC: FieldSpec = FieldSpec::new("pool", 12, 115);
+const SHARD_SPEC: FieldSpec = FieldSpec::new("shard", 8, 107);
+const REVERSE_HASH_SPEC: FieldSpec = FieldSpec::new("reverse_hash", 32, 75);
+const LOCAL_OBJECT_ID_SPEC: FieldSpec = FieldSpec::new("local_object_id", 27, 48);
+const IS_METADATA_SPEC: FieldSpec = FieldSpec::new("is_metadata", 1, 47);
+const OBJECT_CONTENT_SPEC: FieldSpec = FieldSpec::new("object_content", 47, 0);
 
-fn fmt_laddr(laddr: &Laddr) -> String {
-    format!("{:#x}{:016x}", laddr.high, laddr.low)
+const OBJECT_INFO_MASK: u128 =
+    POOL_SPEC.mask() | SHARD_SPEC.mask() | REVERSE_HASH_SPEC.mask() | LOCAL_OBJECT_ID_SPEC.mask();
+const PREFIX_MASK: u128 = UPGRADE_SPEC.mask() | OBJECT_INFO_MASK;
+
+fn parse_laddr(s: &str) -> u128 {
+    if s.starts_with("L") {
+        u128::from_str_radix(&s[1..], 16).unwrap()
+    } else {
+        u128::from_str_radix(&s[..], 16).unwrap()
+    }
 }
 
 #[derive(Tabled)]
@@ -133,52 +60,90 @@ impl LaddrRow {
     fn new(property: &str, value: String) -> Self {
         Self {
             property: property.to_string(),
-            value: value,
+            value,
         }
     }
 }
 
-fn build_laddr_rows(laddr: Laddr) -> Vec<LaddrRow> {
+fn build_laddr_rows(laddr: u128, offset_bits: usize) -> Vec<LaddrRow> {
+    let local_clone_id_spec = FieldSpec::new(
+        "local_clone_id",
+        OBJECT_CONTENT_SPEC.length - offset_bits,
+        offset_bits,
+    );
+    let offset_spec = FieldSpec::new("offset", offset_bits, 0);
     vec![
-        LaddrRow::new("literal", fmt_laddr(&laddr)),
+        LaddrRow::new("laddr", format!("{:x}", laddr)),
+        LaddrRow::new("prefix", format!("{:x}", laddr & PREFIX_MASK)),
+        LaddrRow::new("upgrade", format!("{}", UPGRADE_SPEC.get(laddr) == 1)),
+        LaddrRow::new("pool", format!("{}", POOL_SPEC.get(laddr))),
+        LaddrRow::new("shard", format!("{}", SHARD_SPEC.get(laddr))),
         LaddrRow::new(
-            "object_prefix_no_shadow",
-            fmt_laddr(&laddr.get_object_prefix(false)),
+            "reverse_hash",
+            format!("{:x}", REVERSE_HASH_SPEC.get(laddr)),
         ),
         LaddrRow::new(
-            "object_prefix_has_shadow",
-            fmt_laddr(&laddr.get_object_prefix(true)),
+            "local_object_id",
+            format!("{}", LOCAL_OBJECT_ID_SPEC.get(laddr)),
         ),
-        LaddrRow::new("onode_prefix", fmt_laddr(&laddr.get_onode_prefix())),
-        LaddrRow::new("with_shadow", fmt_laddr(&laddr.with_shadow(true))),
-        LaddrRow::new("without_shadow", fmt_laddr(&laddr.with_shadow(false))),
-        LaddrRow::new("with_metadata", fmt_laddr(&laddr.with_metadata(true))),
-        LaddrRow::new("without_metadata", fmt_laddr(&laddr.with_metadata(false))),
-        LaddrRow::new("with_snap", fmt_laddr(&laddr.with_snap(true))),
-        LaddrRow::new("without_snap", fmt_laddr(&laddr.with_snap(false))),
-        LaddrRow::new("pool", laddr.pool().to_string()),
-        LaddrRow::new("shard", laddr.shard().to_string()),
-        LaddrRow::new("crush", format!("{:#x}", laddr.crush())),
-        LaddrRow::new("random_no_shadow", format!("{:#x}", laddr.random(false))),
-        LaddrRow::new("random_has_shadow", format!("{:#x}", laddr.random(true))),
-        LaddrRow::new("shadow", laddr.shadow().to_string()),
-        LaddrRow::new("metadata", laddr.metadata().to_string()),
-        LaddrRow::new("snap", laddr.snap().to_string()),
-        LaddrRow::new("local_snap_id", laddr.local_snap_id().to_string()),
-        LaddrRow::new("offset", laddr.offset().to_string()),
+        LaddrRow::new(
+            "is_metadata",
+            format!("{}", IS_METADATA_SPEC.get(laddr) == 1),
+        ),
+        LaddrRow::new(
+            "object_content",
+            format!("{:x}", OBJECT_CONTENT_SPEC.get(laddr)),
+        ),
+        LaddrRow::new(
+            "local_clone_id",
+            format!("{:x}", local_clone_id_spec.get(laddr)),
+        ),
+        LaddrRow::new(
+            "offset_16",
+            format!(
+                "{:x}(*4096={:x})",
+                offset_spec.get(laddr),
+                offset_spec.get(laddr) * 4096
+            ),
+        ),
+        LaddrRow::new(
+            "offset",
+            format!(
+                "{}(*4096={})",
+                offset_spec.get(laddr),
+                offset_spec.get(laddr) * 4096
+            ),
+        ),
     ]
 }
 
 fn main() {
     print!("Enter laddr_t: ");
     std::io::stdout().flush().unwrap();
-    let mut input = String::new();
-    std::io::stdin().read_line(&mut input).unwrap();
-    if input.is_empty() {
-        return;
-    }
-    let laddr = Laddr::from(input.trim());
-    let v = build_laddr_rows(laddr);
+
+    let laddr = {
+        let mut laddr = String::new();
+        std::io::stdin().read_line(&mut laddr).unwrap();
+        if laddr.trim().is_empty() {
+            return;
+        }
+        parse_laddr(laddr.trim())
+    };
+
+    print!("Enter offset bits(default 15): ");
+    std::io::stdout().flush().unwrap();
+
+    let offset_bits = {
+        let mut offset_bits = String::new();
+        std::io::stdin().read_line(&mut offset_bits).unwrap();
+        if !offset_bits.trim().is_empty() {
+            offset_bits.trim().parse::<usize>().unwrap()
+        } else {
+            15
+        }
+    };
+
+    let v = build_laddr_rows(laddr, offset_bits);
     let mut t = Table::new(v);
     t.with(Style::sharp());
     println!("{}", t.to_string());
