a6a9b45a286d7bfab75102b07eb9724a181c38c3
zhscn <zhscn@proton.me>
2024-09-12 17:44:21 +0800

    update

--- src/main.rs
+++ src/main.rs
@@ -29,25 +29,29 @@ impl FieldSpec {
     }
 }
 
-// [upgrade:1][pool:12][shard:8][reverse_hash:32][local_object_id:27][is_metadata:1][object_content:47]
 const UPGRADE_SPEC: FieldSpec = FieldSpec::new("upgrade", 1, 127);
-const POOL_SPEC: FieldSpec = FieldSpec::new("pool", 12, 115);
-const SHARD_SPEC: FieldSpec = FieldSpec::new("shard", 8, 107);
-const REVERSE_HASH_SPEC: FieldSpec = FieldSpec::new("reverse_hash", 32, 75);
-const LOCAL_OBJECT_ID_SPEC: FieldSpec = FieldSpec::new("local_object_id", 27, 48);
-const IS_METADATA_SPEC: FieldSpec = FieldSpec::new("is_metadata", 1, 47);
-const OBJECT_CONTENT_SPEC: FieldSpec = FieldSpec::new("object_content", 47, 0);
+const OBJECT_INFO_SPEC: FieldSpec = FieldSpec::new("object_info", 76, 51);
+const OBJECT_CONTENT_SPEC: FieldSpec = FieldSpec::new("object_content", 51, 0);
 
-const OBJECT_INFO_MASK: u128 =
-    POOL_SPEC.mask() | SHARD_SPEC.mask() | REVERSE_HASH_SPEC.mask() | LOCAL_OBJECT_ID_SPEC.mask();
-const PREFIX_MASK: u128 = UPGRADE_SPEC.mask() | OBJECT_INFO_MASK;
+const SHARD_SPEC: FieldSpec = FieldSpec::new("shard", 6, 121);
+const POOL_SPEC: FieldSpec = FieldSpec::new("pool", 12, 109);
+const REVERSE_HASH_SPEC: FieldSpec = FieldSpec::new("reverse_hash", 16, 93);
+const LOCAL_OBJECT_ID_SPEC: FieldSpec = FieldSpec::new("local_object_id", 42, 51);
+const LOCAL_CLONE_ID_SPEC: FieldSpec = FieldSpec::new("local_clone_id", 23, 28);
+const IS_METADATA_SPEC: FieldSpec = FieldSpec::new("is_metadata", 1, 27);
+const BLOCK_OFFSET_SPEC: FieldSpec = FieldSpec::new("block_offset", 27, 0);
+
+const PREFIX_MASK: u128 = UPGRADE_SPEC.mask() | OBJECT_INFO_SPEC.mask();
 
 fn parse_laddr(s: &str) -> u128 {
-    if s.starts_with("L") {
-        u128::from_str_radix(&s[1..], 16).unwrap()
+    let start = if s.starts_with("L") {
+        1
+    } else if s.starts_with("0x") {
+        2
     } else {
-        u128::from_str_radix(&s[..], 16).unwrap()
-    }
+        0
+    };
+    u128::from_str_radix(&s[start..], 16).unwrap()
 }
 
 #[derive(Tabled)]
@@ -65,19 +69,22 @@ impl LaddrRow {
     }
 }
 
-fn build_laddr_rows(laddr: u128, offset_bits: usize) -> Vec<LaddrRow> {
-    let local_clone_id_spec = FieldSpec::new(
-        "local_clone_id",
-        OBJECT_CONTENT_SPEC.length - offset_bits,
-        offset_bits,
-    );
-    let offset_spec = FieldSpec::new("offset", offset_bits, 0);
+fn build_laddr_rows(laddr: u128) -> Vec<LaddrRow> {
+    if laddr & PREFIX_MASK == 0 {
+        return vec![
+            LaddrRow::new("laddr", format!("{:x}", laddr)),
+            LaddrRow::new(
+                "object_content",
+                format!("{:x}", OBJECT_CONTENT_SPEC.get(laddr)),
+            ),
+        ];
+    }
     vec![
         LaddrRow::new("laddr", format!("{:x}", laddr)),
         LaddrRow::new("prefix", format!("{:x}", laddr & PREFIX_MASK)),
         LaddrRow::new("upgrade", format!("{}", UPGRADE_SPEC.get(laddr) == 1)),
-        LaddrRow::new("pool", format!("{}", POOL_SPEC.get(laddr))),
         LaddrRow::new("shard", format!("{}", SHARD_SPEC.get(laddr))),
+        LaddrRow::new("pool", format!("{}", POOL_SPEC.get(laddr))),
         LaddrRow::new(
             "reverse_hash",
             format!("{:x}", REVERSE_HASH_SPEC.get(laddr)),
@@ -87,31 +94,27 @@ fn build_laddr_rows(laddr: u128, offset_bits: usize) -> Vec<LaddrRow> {
             format!("{}", LOCAL_OBJECT_ID_SPEC.get(laddr)),
         ),
         LaddrRow::new(
+            "local_clone_id",
+            format!("{}", LOCAL_CLONE_ID_SPEC.get(laddr)),
+        ),
+        LaddrRow::new(
             "is_metadata",
             format!("{}", IS_METADATA_SPEC.get(laddr) == 1),
-        ),
-        LaddrRow::new(
-            "object_content",
-            format!("{:x}", OBJECT_CONTENT_SPEC.get(laddr)),
-        ),
-        LaddrRow::new(
-            "local_clone_id",
-            format!("{:x}", local_clone_id_spec.get(laddr)),
         ),
         LaddrRow::new(
             "offset_16",
             format!(
                 "{:x}(*4096={:x})",
-                offset_spec.get(laddr),
-                offset_spec.get(laddr) * 4096
+                BLOCK_OFFSET_SPEC.get(laddr),
+                BLOCK_OFFSET_SPEC.get(laddr) * 4096
             ),
         ),
         LaddrRow::new(
-            "offset",
+            "offset_10",
             format!(
                 "{}(*4096={})",
-                offset_spec.get(laddr),
-                offset_spec.get(laddr) * 4096
+                BLOCK_OFFSET_SPEC.get(laddr),
+                BLOCK_OFFSET_SPEC.get(laddr) * 4096
             ),
         ),
     ]
@@ -130,20 +133,7 @@ fn main() {
         parse_laddr(laddr.trim())
     };
 
-    print!("Enter offset bits(default 15): ");
-    std::io::stdout().flush().unwrap();
-
-    let offset_bits = {
-        let mut offset_bits = String::new();
-        std::io::stdin().read_line(&mut offset_bits).unwrap();
-        if !offset_bits.trim().is_empty() {
-            offset_bits.trim().parse::<usize>().unwrap()
-        } else {
-            15
-        }
-    };
-
-    let v = build_laddr_rows(laddr, offset_bits);
+    let v = build_laddr_rows(laddr);
     let mut t = Table::new(v);
     t.with(Style::sharp());
     println!("{}", t.to_string());
